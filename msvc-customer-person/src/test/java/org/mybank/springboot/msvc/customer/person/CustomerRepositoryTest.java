6abc283cc2e4faa41aca2f5821caa905397ed5ed
JorgeToasaY <medardotoasa@yahoo.com>
2025-08-29 17:20:03 -0500

    ejercicio practico

---
+++ msvc-customer-person/src/test/java/org/mybank/springboot/msvc/customer/person/CustomerRepositoryTest.java
@@ -0,0 +1,30 @@
+package org.mybank.springboot.msvc.customer.person;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mybank.springboot.msvc.customer.person.entity.Customer;
+import org.mybank.springboot.msvc.customer.person.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(SpringExtension.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DataJpaTest
+class CustomerRepositoryTest {
+    @Autowired
+    CustomerRepository repo;
+
+    @Test
+    void shouldSaveAndFindByCustomerId() {
+        Customer c = new Customer();
+        c.setName("Ana"); c.setGender("F"); c.setAge(30);
+        c.setIdentification("123"); c.setCustomerId("C-001");
+        c.setPassword("pwd"); c.setState(true);
+        repo.save(c);
+        assertTrue(repo.findByCustomerId("C-001").isPresent());
+    }
+}
